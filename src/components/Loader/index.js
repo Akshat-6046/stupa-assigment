af2c6ff1f6150247ff67909a1f5333b049c9a725
Akshat-6046 <109698899+Akshat-6046@users.noreply.github.com>
2024-04-18 16:48:09 +0530

    Update index.js

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from './styles.module.css'
 function Loader() {
   return (
-    <div className={styles.loader}>Loader...</div>
+    <div className={styles.loader} />
   )
 }
 
